5848f7a6100e26261d749400e03d025d59518c6e
langonginc <langonginc@yeah.net>
2020-04-15 14:00:39 +0800

    Update

--- install.sh
+++ install.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-cd ~/
-# 1. Download
-git clone 'https://github.com/langonginc/Timer.git'
 
-# 2. Setting
-al="alias timer='bash ~/Timer/timer.sh'"
-echo $al >> ~/.bashrc
+clear
+
+whiptail --title "Installer" --msgbox "Welcome to LanGong-Timer Installer" 10 60
+
+npath=$(dirname $(readlink -f "$0"))
+
+cp timer.sh ~/
+
+cd ~/
+
+{
+    for ((i = 0 ; i <= 100 ; i+=33)); do
+        sleep 1
+        echo $i
+    done
+} | whiptail --gauge "Installing" 6 60 0
+whiptail --title "Installer" --msgbox "Done!" 10 60
+
+clear
--- timer.sh
+++ timer.sh
@@ -3,7 +3,7 @@
 clear
 
 
-PET=$(whiptail --title "Input time (seconds), must be numbers." --inputbox "Input a number." 10 60  3>&1 1>&2 2>&3)
+PET=$(whiptail --title "Timer" --inputbox "Input time (seconds), must be numbers." 10 60  3>&1 1>&2 2>&3)
 exitstatus=$?
 if [ $exitstatus = 0 ]; then
 
@@ -11,24 +11,34 @@ rd=$PET
 clear
 if [ "$rd" == "" ]
 then
-	echo -e "\e[36m >> Timer >> \e[31mError-01: Pleese input time.\e[31m"
+	whiptail --title "Timer" --msgbox "Please input the time!" 10 60
 	exit 1
 fi
 clear
 if [ "$rd" = "0" ]
 then
-	whiptail --title "Time is over." --msgbox "The Time is over!" 10 60
+	whiptail --title "Timer" --msgbox "The Time is over!" 10 60
 	exit 0
 fi
 
 add=$((100/$rd))
+if [ $((100/$rd)) > $((0)) ]
+then 
+	add=$(printf "%.5f" `echo "scale=5;100/$rd"|bc`)
+fi
 {
-    for ((i = 0 ; i <= 100 ; i+=$add)); do
+    for ((i = 0 ; i <= $rd ; i+=1)); do
         sleep 1
-        echo $i
+        echo $(printf "%.0f" `echo "scale=5;$i*$add"|bc`)
     done
 } | whiptail --gauge "Timer" 6 60 0
-	whiptail --title "Time is over." --msgbox "The Time is over!" 10 60
+	whiptail --title "Timer" --msgbox "Time is over!" 10 60
 else
     echo "You chose Cancel."
 fi
+
+###### Made By LanGongINC ###### 
+
+###### Copyritht 2020 LanGongINC ######
+
+###### Made with GitHun and Git ######
